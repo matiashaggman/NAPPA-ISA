6139f66da6d3c618f4df46e435243f5c5999d33d
Matias Häggman <matias.haggman@gmail.com>
2025-02-05 20:04:12 +0200

    version 1.0

---
+++ nappa/models.py
@@ -0,0 +1,194 @@
+import os
+import torch
+import torch.nn as nn
+from concurrent.futures import ProcessPoolExecutor
+
+
+class NappaModel(nn.Module):
+    def predict(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Predict labels for the given features of a sleep recording.
+
+        Args:
+            x (Tensor): The feature matrix of a sleep recording.
+
+        Returns:
+            Tensor: Probability distribution of the classes at each time step and the predicted classes.
+        """
+        self.eval()
+        with torch.no_grad():
+            output = self.forward(x)
+
+            # Predicted probability distribution on classes for each timestep
+            p_dist = torch.softmax(output, dim=-1)
+            
+            # Predicted class for each timestep (integer)
+            p_classes = torch.argmax(p_dist, dim=-1)
+
+        return torch.cat([p_classes.unsqueeze(-1), p_dist], dim=1)
+
+    def load(self, weight_file: str):
+        self.load_state_dict(torch.load(weight_file, map_location=self.device, weights_only=True))
+        return self
+
+    def reset(self):
+        """
+        Reset the parameters of the model.
+        """
+        for layer in self.children():
+            if hasattr(layer, 'reset_parameters'):
+                layer.reset_parameters()
+        return self
+
+    @property
+    def device(self) -> torch.device:
+        return next(self.parameters()).device
+
+    @property
+    def num_parameters(self) -> int:
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)  
+
+
+class NappaSleepNet(NappaModel):
+    def __init__(self, num_features=5, num_classes=3, hidden_size=10, num_layers=2,
+                  bidirectional=True, name='NappaSleepNet'):
+        """
+        Initialize the NappaSleepNet model for classifying sleep stages.
+        # NOTE: The model is initialized by default with the same hyperparameters as used in the article
+         https://doi.org/10.1016/j.heliyon.2024.e33295
+        Args:
+          n_features (int): The number of features for each time step in the input.
+          n_classes (int): The number of sleep stages for classification.
+          hidden_size (int): The number of dimensions in the hidden state (Capacity of the memory of the model)
+          num_layers (int): The number of GRU units.
+          padding_value (int): The padding value used to fill the tensor to match the longest 
+          sleep sequence in each batch during the training of the classifier.
+        """
+        super().__init__()
+        self.name = name
+  
+        self.gru = nn.GRU(input_size=num_features, hidden_size=hidden_size,
+                          batch_first=True, bidirectional=bidirectional, num_layers=num_layers)
+
+        # Define the output layer that maps the hidden state to class logits.
+        # If bidirectional, the hidden size is doubled as it concatenates the features from both directions.
+        self.out = nn.Linear(in_features=hidden_size*2 if bidirectional else hidden_size, 
+                             out_features=num_classes)
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass through the network.
+        
+        Args:
+          x (Tensor): The input tensor containing features of shape (batch_size, rec_length, n_features).
+          rec_lengths (Tensor): The actual lengths of each sleep sequence before padding.
+
+        Returns:
+          Tensor: The output logits of shape (batch_size, rec_length, n_classes).
+        """
+        # Pass the input through the GRU layer.   
+        y_gru, _ = self.gru(x)
+        
+        # Pass the output of the GRU through the output layer to get class logits for each time step.
+        y = self.out(y_gru)
+        
+        return y
+
+        
+class EnsembleClassifier:
+    def __init__(self, model_class, weights_dir, model_args= {
+                            'num_features'  :5,
+                            'num_classes'   :3,
+                            'hidden_size'   :10,
+                            'num_layers'    :2,
+                            'bidirectional' :True
+                        }):
+        """
+        Initialize the general ensemble model. This can be used to construct an ensemble model
+        from the cross validated NappaSleepNet models (weight files). This classifier produces
+        predicted sleep classes by voting & averaging the model output distributions.
+
+        Args:
+          model (NappaSleepNet): Sleep classifier base class
+          model_args (dict): Dictionary containing the initialization hyperparameters
+          weights_dir (str): Directory where the pytorch model files (weights, .pth) are stored.
+        """
+
+        self.model_class = model_class
+        self.model_args = model_args
+        self.weights_dir = weights_dir
+        self.i = 0
+        # Load models in parallel
+        self.models = self._load_models_in_parallel()
+
+
+    def _load_single_model(self, weight_file):
+        weight_file_path = os.path.join(self.weights_dir, weight_file)
+        model = self.model_class(**self.model_args)  # Initialize new instance for each weight file
+        model.load(weight_file_path)
+        model.eval()
+        return model
+
+    def _load_models_in_parallel(self):
+        weight_files = os.listdir(self.weights_dir)
+
+        with ProcessPoolExecutor() as executor:
+            models = list(executor.map(self._load_single_model, weight_files))
+
+        return models
+
+    def _predict_single_model(self, model, x):
+        with torch.no_grad():
+            preds = model.predict(x)[:, 0]  # First column contains the predicted sleep stages
+            dists = model.predict(x)[:, 1:] # Rest columns contain the stage probabilities
+            
+
+        return preds, dists
+    
+    def predict(self, x, progress_callback=None, status_callback=None) -> torch.Tensor:
+        """
+        Predict the class for each time step using ensemble voting and classifier output averaging.
+
+        Args:
+          x (Tensor): The input tensor containing features of shape (rec_length, n_features).
+
+        Returns:
+          Tensor: The predicted classes and probability distributions of shape (rec_length, 1 + num_classes).
+        """
+        if status_callback:
+            status_callback('Status: computing predictions...')
+        # Parallelize predictions for all models using ProcessPoolExecutor
+        with ProcessPoolExecutor() as executor:
+            results = list(executor.map(self._predict_single_model, self.models, [x] * len(self.models)))
+
+        all_predictions = []
+        all_dists = []
+
+        for i, (preds, dists) in enumerate(results):
+            all_predictions.append(preds)
+            all_dists.append(dists)
+
+        # Stack the predictions to a tensor of shape (num_models, rec_length),
+        # and transpose it to shape (rec_length, num_models) for majority voting.
+        all_predictions = torch.transpose(torch.stack(all_predictions), 0, 1).type(torch.long)
+        all_dists = torch.stack(all_dists)
+
+        # Construct the predicted class vector by majority voting:
+        voted_predictions = []
+        for i in range(all_predictions.shape[0]):  # Iterate through each time step
+            prediction = all_predictions[i]
+            class_freq = torch.bincount(prediction)  # Count the frequency of each class
+            most_popular_class = torch.argmax(class_freq)  # Get the most popular class
+            voted_predictions.append(most_popular_class)
+
+        voted_predictions = torch.tensor(voted_predictions)
+
+        # Construct the predicted 'pseudo' distribution vector by averaging all the predictions
+        mean_dists = torch.mean(all_dists, dim=0)
+
+        # Scale the averaged distributions to add up to 1.
+        normalized_mean_dists = torch.zeros((mean_dists.shape))
+        for i in range(normalized_mean_dists.shape[0]):
+            normalized_mean_dists[i] = mean_dists[i] / torch.sum(mean_dists[i])
+
+        return torch.cat([voted_predictions.unsqueeze(-1), normalized_mean_dists], dim=1)
---
+++ nappa/pipeline.py
@@ -0,0 +1,347 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+
+from .objects import NappaDataset, SleepRecording
+
+def read_and_process_hypnogram(hypno_path, cfg):
+    
+    """
+    Reads and processes a hypnogram file.
+    
+    Args:
+        hypno_path (str): The file path to the hypnogram file.
+    
+    Returns:
+        pd.DataFrame: A DataFrame with processed datetime and sleep stage information.
+    """
+        
+    with open(hypno_path, 'r') as file:
+        lines = file.readlines()
+
+    # Extract start date
+    start_date_str = lines[1].split('Start Time: ')[1].strip()
+    start_date = pd.to_datetime(start_date_str, format='%d.%m.%Y %H:%M:%S').date()
+
+    # Skip header
+    hypnogram_df = pd.read_table(hypno_path, skiprows=8, delimiter= ' ', header=None)
+    
+    # If each timestamp contains also date
+    if hypnogram_df.shape[1] == 3:
+        hypnogram_df.columns = ['date', 'time', 'sleep_stage']
+        hypnogram_df['date'] = pd.to_datetime(hypnogram_df['date'], format='%d.%m.%Y')
+       
+        if ',000' in str(hypnogram_df['time'][0]):
+            # Standard output form from clinical hypnograms
+            hypnogram_df['time'] = pd.to_datetime(hypnogram_df['time'], format='%H:%M:%S,%f;')
+        else:
+            # Pseudo hypnogram form
+            hypnogram_df['time'] = pd.to_datetime(hypnogram_df['time'].str.strip(','), format='%H:%M:%S')
+
+
+        hypnogram_df['timestamp'] = hypnogram_df['date'] + (hypnogram_df['time'] - hypnogram_df['time'].dt.normalize())
+    # Else prepend date to each timestamp
+    elif hypnogram_df.shape[1] == 2:
+
+        hypnogram_df.columns = ['time', 'sleep_stage']
+        hypnogram_df['time'] = pd.to_datetime(hypnogram_df['time'], format='%H:%M:%S,%f;').dt.time
+        
+        hypnogram_df['date'] = start_date
+        hypnogram_df['timestamp'] = pd.to_datetime(hypnogram_df['date'].astype(str) + ' ' + hypnogram_df['time'].astype(str))
+        
+        # Look for timestamps after midnight
+        rollovers = (hypnogram_df['timestamp'].diff() < pd.Timedelta(0)).cumsum()
+        
+        # Change the date after midnight
+        hypnogram_df['timestamp'] += pd.to_timedelta(rollovers, unit='D')
+
+    hypnogram_df = hypnogram_df[['timestamp', 'sleep_stage']]
+    hypnogram_df['timestamp'] = pd.to_datetime(hypnogram_df['timestamp'])  # Convert 'datetime' column to datetime type
+    hypnogram_df = hypnogram_df.set_index('timestamp')  # Set 'datetime' column as the index
+    hypnogram_df = hypnogram_df[['sleep_stage']]
+    # Interpolate Artifacts (A) with the previous sleep stage. TODO: Maybe Find a better way to handle hypnogram epoch artifacts (?).
+    hypnogram_df['sleep_stage'] = hypnogram_df['sleep_stage'].replace('A', None).ffill()
+
+    # Resample hypnogram to wanted time resolution.
+    hypnogram_df = hypnogram_df.resample(cfg['time_resolution']).ffill().bfill()
+
+    return hypnogram_df
+
+
+def align_features_with_labels(sensor_df, hypnogram_df, cfg):
+    """
+    Aligns features with labels based on timestamps.
+    
+    Args:
+        sensor_df (pd.DataFrame): DataFrame with sensor features.
+        hypnogram_df (pd.DataFrame): DataFrame with a processed hypnogram.
+    
+    Returns:
+        Tuple: Temporally aligned features and hypnogram DataFrames.
+    """
+    # Align indices by finding the intersection of timestamps
+    common_index = sensor_df.index.intersection(hypnogram_df.index)
+    
+    # Filter dataframes to the common indices
+    aligned_sensor_df = sensor_df.loc[common_index]
+    aligned_hypnogram_df = hypnogram_df[hypnogram_df.index.isin(common_index)]
+    
+    # Ensure the length of both DataFrames are the same
+    min_length = min(len(aligned_sensor_df), len(aligned_hypnogram_df))
+    aligned_sensor_df = aligned_sensor_df.iloc[:min_length]
+    aligned_hypnogram_df = aligned_hypnogram_df.iloc[:min_length]
+
+    return aligned_sensor_df, aligned_hypnogram_df
+
+
+def resample_features(acc_df, gyro_df, cfg):
+    """
+    Resamples sensor features to match the wanted frequency (usually hypnogram's 30-second epochs).
+    
+    Args:
+        acc_df (pd.DataFrame): DataFrame with accelerometer derived features.
+        gyro_df (pd.DataFrame): DataFrame with gyroscope derived features.
+    
+    Returns:
+        pd.DataFrame: A DataFrame with combined and resampled features.
+    """
+
+    if cfg is not None:
+        sample_interval=cfg['time_resolution']
+    else:
+        sample_interval='30s'
+
+    # Resample gyroscope features to exactly 30s intervals to match hypnogram labels
+    resampled_gyro_df = gyro_df.resample(sample_interval).mean().interpolate(method='spline', order=3, s=0.)
+
+    # Resample accelerometer features to 30s intervals by grouping and averaging
+    resampled_acc_df = acc_df.resample(sample_interval).mean()
+
+    # Ensure both DataFrames have the same index
+    resampled_acc_df = resampled_acc_df.reindex(resampled_gyro_df.index).interpolate(method='spline', order=3, s=0.)
+
+    # Combine the resampled DataFrames (by columns)
+    feature_df = pd.concat([resampled_acc_df, resampled_gyro_df], axis=1)
+
+    return feature_df
+
+
+def rename_columns(df, format):
+    """
+    Renames the columns of the feature dataframe according to standard convention.
+    """
+    if format == 'legacy':
+        df.rename(columns={'feature1(class N)':'body_pos', 'feature2(m/sec)': 'activity',
+                    'feature7(m/sec^2)':'actigraph', 'feature3_X(unitless)': 'acf_max_x',
+                    'feature3_Y(unitless)': 'acf_max_y','feature4_X(Hz)': 'resp_rate_x',
+                    'feature4_Y(Hz)': 'resp_rate_y','feature5_Y(grad/sec)': 'resp_peak_median',
+                    'feature6_Y(grad/sec)': 'resp_peak_std'}, inplace=True)
+    else:
+        df.rename(columns={'BodyPosValue':'body_pos', 'ActivityValue':'activity', 
+                    'ActigraphValue':'actigraph', 'ACFMaxValue_X':'acf_max_x',
+                    'ACFMaxValue_Y':'acf_max_y', 'RespRate_X':'resp_rate_x', 'RespRate_Y':'resp_rate_y',
+                    'RespPeakMed_Y':'resp_peak_median', 'RespPeakStd_Y':'resp_peak_std'}, inplace=True)
+    return df
+
+
+def select_default_features(features, format='standard'):
+    """
+    Selects the default 5 features for classification.
+    """
+
+    if type(features) == pd.DataFrame and format == 'legacy':
+        features = features[['feature2(m/sec)','feature3_Y(unitless)','feature4_Y(Hz)',
+                             'feature5_Y(grad/sec)', 'feature6_Y(grad/sec)']]
+    elif type(features) == pd.DataFrame and format == 'new':
+        features = features[['ActivityValue','ACFMaxValue_Y','RespRate_Y',
+                             'RespPeakMed_Y', 'RespPeakStd_Y']]
+        
+    elif type(features) == pd.DataFrame and format == 'standard':
+        features = features[['activity','acf_max_y','resp_rate_y','resp_peak_median', 'resp_peak_std']]
+    return features
+
+def read_and_process_features(acc_path, gyro_path, time_offset, cfg=None):
+    """
+    Reads sensor features from CSV files and processes them for classification.
+    
+    Args:
+        acc_path (str): Path to the accelerometer feature CSV file.
+        gyro_path (str): Path to the gyroscope feature CSV file.
+        time_offset (int): Time offset from UTC in hours.
+    
+    Returns:
+        pd.DataFrame: A DataFrame with sensor features ready for classification.
+    """
+
+    file = open(acc_path, 'r')
+    first_line = file.readline()
+    file.close()
+
+    # Legacy csv format
+    if 'UTCTimestamp(ms)' in first_line:
+        skiprows = 0
+        skipfooter = 0
+        unit = 'ms'
+        timeColName = 'UTCTimestamp(ms)'
+        format = 'legacy'
+    else: # New format
+        skiprows = 1
+        skipfooter = 4
+        unit = 's'
+        timeColName = 'Timestamp'
+        format = 'new'
+
+    accData = pd.read_csv(acc_path, skipinitialspace=True, skiprows=skiprows, skipfooter=skipfooter, engine='python')
+    gyroData = pd.read_csv(gyro_path, skipinitialspace=True, header=skiprows, skipfooter=skipfooter, engine='python')
+    
+    # Convert timestamps to local time using the time offset
+    accTime  = pd.to_datetime(accData[timeColName],  unit=unit, utc=True) + pd.Timedelta(hours=time_offset)
+    gyroTime = pd.to_datetime(gyroData[timeColName], unit=unit, utc=True) + pd.Timedelta(hours=time_offset)
+
+    # Set time based indexing
+    accData = accData.set_index(accTime)
+    gyroData = gyroData.set_index(gyroTime)
+
+    # Resample features to constant 30s to match hypnogram
+    feature_df = resample_features(accData, gyroData, cfg=cfg)
+    feature_df.index.name = 'timestamp'
+    feature_df.drop(columns=[timeColName], inplace=True)
+    feature_df = rename_columns(feature_df, format)
+    if cfg is not None:
+        if cfg['default_features']:
+            feature_df = select_default_features(feature_df, format='standard')
+
+    return feature_df
+
+
+def read_subject_info(info_path):
+    """
+    Reads subject information from a JSON file.
+
+    Args:
+        info_path (str): Path to the subject info JSON file.
+    
+    Returns:
+        dict: A dictionary containing subject information.
+    """
+    with open(info_path, 'r') as file:
+        subject_info = json.load(file)
+    return subject_info[0]
+
+
+def compile_recording(acc_path, gyro_path, hypno_path, info_path, cfg):
+    """
+    Compiles a single sleep recording from sensor and hypnogram data.
+
+    Args:
+        acc_path (str): Path to the accelerometer data file.
+        gyro_path (str): Path to the gyroscope data file.
+        hypno_path (str, optional): Path to the hypnogram data file.
+        info_path (str, optional): Path to the subject info file.
+    
+    Returns:
+        SleepRecording: An instance of SleepRecording with features and labels.
+    """
+    # Read subject info from JSON file
+    if info_path:
+        subject_info = read_subject_info(info_path)
+    else:
+        subject_info = {}
+    
+    if subject_info.get('subject_age') == None:
+        age=1
+    else:
+        age=subject_info.get('subject_age')
+    
+    if subject_info.get('location'):
+        time_zone=subject_info.get('location')
+    else:
+        time_zone='Europe/Helsinki'
+
+    # Load feature data
+    sensor_features_df = read_and_process_features(acc_path, gyro_path,
+                                                    time_zone=time_zone, cfg=cfg)
+
+    # Load hypnogram data
+    hypnogram_df = read_and_process_hypnogram(hypno_path, cfg=cfg)
+
+    # Align sensor features with hypnogram labels
+    #if not raw_format:
+    sensor_features_df, hypnogram_df = align_features_with_labels(sensor_features_df,
+                                                                hypnogram_df,
+                                                                cfg=cfg)
+
+    # Convert data to numpy arrays
+    features = sensor_features_df
+    labels   = hypnogram_df
+
+    compiledRecording = SleepRecording(
+            features    = features,
+            labels      = labels,
+            id          = subject_info.get('subject_id'),
+            clinical_id = subject_info.get('clinical_id'),
+            age         = age,
+            comments    = subject_info.get('comments')
+        )
+    return compiledRecording
+
+
+def scan_directory(path, cfg):
+    """
+    Scans a directory for accelerometer, gyroscope, hypnogram and info files.
+    
+    Args:
+        path (str): Directory path to scan.
+    
+    Returns:
+        Tuple: Paths to the accelerometer, gyroscope, and hypnogram and subject info files.
+    """
+    acc_file, gyro_file, hypno_file, info_file = None, None, None, None
+
+    for file in os.listdir(path):
+        if file.endswith('.csv') and 'AccFeatures' in file:
+                acc_file = file
+        elif file.endswith('.csv') and 'GyroFeatures' in file:
+                gyro_file = file
+        elif (('sleep' in file) or ('profile' in file) or ('hypnogram' in file)):
+            hypno_file = file
+        elif file.endswith('.json'):
+            info_file = file
+        else:
+            continue
+
+    return (acc_file, gyro_file, hypno_file, info_file)
+
+
+def create_dataset(root, cfg={'default_features':True, 'time_resolution':'30s'}):
+    """
+    Creates a dataset by scanning a parent directory for subdirectories containing all relevant sleep recording files.
+    
+    Args:
+        root (str): The root directory containing subdirectories of sleep recordings.
+    
+    Returns:
+        NappaDataset: A dataset containing multiple SleepRecording instances.
+    """ 
+
+    directories = [entry.name for entry in os.scandir(root) if entry.is_dir()]
+    recordings = []
+
+    for directory in directories:
+
+        path = os.path.join(root, directory)
+        
+        acc_file, gyro_file, hypno_file, info_file = scan_directory(path, cfg=cfg)
+        if None in [acc_file, gyro_file, hypno_file, info_file]:
+            continue
+            
+        recording = compile_recording(acc_path  =os.path.join(path, acc_file),
+                                      gyro_path =os.path.join(path, gyro_file),
+                                      hypno_path=os.path.join(path, hypno_file),
+                                      info_path =os.path.join(path, info_file),
+                                      cfg=cfg)
+        recordings.append(recording)
+
+
+    return NappaDataset(data=recordings)
